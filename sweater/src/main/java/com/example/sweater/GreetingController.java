5c492036e6cd1f5ee7b35602fe8f0c106b5c15fc
Dmitry Kartashov <dmitrykart4shov@gmail.com>
2021-05-04 21:01:37 +0300

    added app sweater SpringMVC

--- sweater/src/main/java/com/example/sweater/GreetingController.java
+++ sweater/src/main/java/com/example/sweater/GreetingController.java
@@ -2,16 +2,19 @@ package com.example.sweater;
 
 import com.example.sweater.entities.Message;
 import com.example.sweater.repos.MessageRepos;
+import com.sun.org.omg.SendingContext._CodeBaseImplBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sun.security.provider.certpath.DistributionPointFetcher;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -40,6 +43,21 @@ public class GreetingController {
         Iterable<Message> messages = messageRepos.findAll();
         model.put("messages", messages);
         return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String text, Map<String, Object> model) {
+
+        Iterable<Message> messages;
+
+        if (text != null && !text.isEmpty()) {
+            messages = messageRepos.findByTag(text);
+        } else {
+            messages = messageRepos.findAll();
+        }
+            model.put("messages", messages);
+        return "main";
+
     }
 
 }
