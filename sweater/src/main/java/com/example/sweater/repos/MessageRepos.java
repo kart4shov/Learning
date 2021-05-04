42cdecf70d335a6500b59176043133d3e9dc27fa
Dmitry Kartashov <dmitrykart4shov@gmail.com>
2021-05-04 21:28:51 +0300

    deleting projects

--- sweater/src/main/java/com/example/sweater/repos/MessageRepos.java
+++
@@ -1,7 +0,0 @@
-package com.example.sweater.repos;
-
-import com.example.sweater.entities.Message;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepos extends CrudRepository<Message, Long> {
-}
